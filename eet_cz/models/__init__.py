e0f234b1678e3af8dcf3d2b12ac2c3adb1b9e784
Amevia <66975640+Amevia-sro@users.noreply.github.com>
2021-10-24 17:16:01 +0200

    Delete eet_cz directory

--- eet_cz/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import eet_message
-from . import pos
-from . import product
-from . import res_partner
-from . import res_company
-from . import revenue_data_message
--- eet_cz/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import eet_connection_test
-from . import pos_payment
-
